39dd33be0289d51ab35ea60af2f590b5b54df605
youngkim90 <ceo.youngkim@gmail.com>
2021-12-25 00:37:37 +0900

    OAuth2 설정

---
+++ src/test/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final SecurityConfig customOauth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/","/css/**", "/images/**","/js/**","/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout().logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login().userInfoEndpoint().userService((OAuth2UserService<OAuth2UserRequest, OAuth2User>) customOauth2UserService);
+    }
+}
